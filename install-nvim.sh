5a3fe48f49b54e9d484ff82241b8661f9bd0f33e
zl <zl03jsj@163.com>
2022-10-28 00:03:20 +0800

    use vim-plug instead of vim-vundle

--- install-nvim.sh
+++ install-nvim.sh
@@ -11,6 +11,7 @@
 #brew install pkg-config
 
 #npm install --global yarn
+#pip3 install pynvim
 
 #sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 #       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
