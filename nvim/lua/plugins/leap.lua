a9d38c0377bb124959321eb7ca8d843ba9696887
leo <zl03jsj004@gmail.com>
2026-02-24 15:13:20 +0800

    feat(leap): remove key mapping in x mode

--- nvim/lua/plugins/leap.lua
+++ nvim/lua/plugins/leap.lua
@@ -9,11 +9,12 @@ local M = {
     -- 2. 设置 "Sneak-style" 映射 (s/S)
     -- 这让你使用 s 向前跳转，S 向后跳转
     leap.add_repeat_matches_by_direction = true
-    vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap-forward-to)', { desc = 'Leap forward to' })
-    vim.keymap.set({'n', 'x', 'o'}, 'S', '<Plug>(leap-backward-to)', { desc = 'Leap backward to' })
+    -- vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap-forward-to)', { desc = 'Leap forward to' })
+    -- vim.keymap.set({'n', 'x', 'o'}, 'S', '<Plug>(leap-backward-to)', { desc = 'Leap backward to' })
     -- 3. 设置 "Exclusive" 映射 (用于操作符模式，如 x/X)
     -- 这模仿了 Sneak 的 x/X 行为，在执行删除/修改操作时非常有用
-    vim.keymap.set({'n', 'x', 'o'}, 'gs', '<Plug>(leap-from-window)', { desc = 'Leap from window' })
+    -- vim.keymap.set({'n', 'x', 'o'}, 'gs', '<Plug>(leap-from-window)', { desc = 'Leap from window' })
+    vim.keymap.set({'n'}, 'gs', '<Plug>(leap-from-window)', { desc = 'Leap from window' })
     vim.keymap.set('n', 's', '<Plug>(leap-anywhere)')
   end
 }
